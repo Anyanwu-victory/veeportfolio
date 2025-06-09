47a2798d4e4fedcf53389c7d72174d283fad1adf
Anyanwu-victory <118054739+Anyanwu-victory@users.noreply.github.com>
2025-06-09 22:44:27 +0100

    remove unused variable

--- components/sections/contact-section.tsx
+++ components/sections/contact-section.tsx
@@ -2,7 +2,6 @@
 
 import type React from "react"
 import { useEffect } from "react"
-import { useState } from "react"
 import { motion } from "framer-motion"
 import { Card, CardContent } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
